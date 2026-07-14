3e4bd8bbb74a3ea5037bb3a0e21ad923d05bcbe8
iamabdoulaziz <abdoulazizc867@gmail.com>
2026-07-14 10:53:57 +0000

    component header for admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ListSymptomesComponent } from './Pages/symptomes/list-symptomes/list-symptomes';
+import { Header } from './Component/header/header';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'symptomes', pathMatch: 'full' },
-  { path: 'symptomes', component: ListSymptomesComponent }
+  { path: 'symptomes', component: ListSymptomesComponent },
+  {path: 'navbar', component: Header}
 ];
