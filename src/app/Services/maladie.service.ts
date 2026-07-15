fefc36b245eebdc4818223c22506bc197a1cbd83
Awa Koniba <konibaawa@gmail.com>
2026-07-15 17:34:16 +0000

    Modif

---
+++ src/app/Services/maladie.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, signal } from "@angular/core";
+import { Maladie } from "../Models/maladie.model";
+
+@Injectable({
+    providedIn :'root'
+})
+export class MaladieService {
+
+    Maladies = signal<Maladie[]>([
+        {
+            id: 1,nom: 'Paludisme',description: 'grave',dateDeclaration: '5/07/2026'
+        }
+    ])
+}
