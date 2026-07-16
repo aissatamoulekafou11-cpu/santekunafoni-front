6163a02b11a6d22f97ff41e14bc69c763d657411
DOUYON <lc6issa@gmail.com>
2026-07-16 09:19:59 +0000

    modif

--- src/app/Pages/agent-dashboard/agent-dashboard.ts
+++ src/app/Pages/agent-dashboard/agent-dashboard.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar-component/sidebar-component';
 import { Header } from '../../Component/header/header';
+
 @Component({
   selector: 'app-agent-dashboard',
   standalone: true,
   imports: [
-    SidebarComponent,Header
+    SidebarComponent,
+    Header
   ],
   templateUrl: './agent-dashboard.html',
   styleUrl: './agent-dashboard.css'
 })
 export class AgentDashboard {
 
+  // Cartes statistiques
   nombrePatients = 0;
   nombreSymptomes = 0;
   nombreMaladies = 0;
   nombreTraitements = 0;
+
+  // Répartition par sexe
+  pourcentageHommes = 0;
+  pourcentageFemmes = 0;
+
+  // Données des tableaux
+  derniersPatients: any[] = [];
+
+  topSymptomes: any[] = [];
 
 }
