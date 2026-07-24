e40e91d13378dc30854a4f122c293a400fc697f3
Oumar Kanadji <oumarbambakanadji@gmail.com>
2026-07-24 17:09:50 +0000

    remise en place du sidebar dans app.html

--- src/app/Component/sidebar/sidebar.ts
+++ src/app/Component/sidebar/sidebar.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faGaugeHigh, faUserNurse, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
